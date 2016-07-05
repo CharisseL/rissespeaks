a4f8325113a1a7345eeb9ea968ccfb1a37803508
CharisseL <charisse8@gmail.com>
2016-07-05 15:47:45 -0500

    setup admin page for superusers

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from collection.models import Story
+
+class StoryAdmin(admin.ModelAdmin):
+    model = Story
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
+admin.site.register(Story, StoryAdmin)
